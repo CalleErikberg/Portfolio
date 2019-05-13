f4ae1e531984d6c42b62636441543d23ccdcc4f2
Erik Calleberg <erik.calleberg@gmail.com>
2019-05-13 12:42:29 +0200

    javashit

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,20 +1,26 @@
 import React, { Component } from "react"
 
-
 class Card extends Component {
-  openModal() {
+openModal() {
 var modal = document.getElementById('myModal'); // Get the button that opens the modal
-var img = document.getElementById("open-modal"); // Get the <span> element that closes the modal
+var img = document.getElementsByClassName("open-modal"); // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0]; // When the user clicks the button, open the modal 
-
+// When the user clicks on the button, open the modal
+for(var i = 0; i < img.length; i++) {
+  img[i].onclick = function () {
+      modal.style.display = "block";
+  }
+}
+/*
 img.onclick = function () {
   modal.style.display = "block";
-}; // When the user clicks on <span> (x), close the modal
-
+}; 
+*/
+// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-}; // When the user clicks anywhere outside of the modal, close it
-
+}; 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -24,8 +30,7 @@ window.onclick = function (event) {
   render() {
     return (
       <div className="container">
-        <div className="front">
-        <img id="open-modal" onClick={() => this.openModal()} src={this.props.pic} />
+        <img className="open-modal" onClick={() => this.openModal()} src={this.props.pic} />
           <div id="myModal" class="modal">
             <div class="modal-content">
               <h3 className="caption">{this.props.title}</h3>
@@ -39,7 +44,6 @@ window.onclick = function (event) {
             </div>
           </div>
         </div>
-      </div>
     )
   }
 }
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -12,13 +12,13 @@ class Cards extends Component {
         </p>
         <div id="content">
           <Card
-            pic="https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            pic="https://media-eu.jobylon.com/CACHE/companies/company-logo/demo-sverige/coop_logo_sweden_green.9d2a1c4e/1849c21895bf81009bb55e97d2bfb751.jpg"
             title="COOP - Service Design"
             firstText="The work has been carried out for the food company Coop Sverige AB. The group started the project by participating in two teacher-led workshops to prepare the group members for the real work. The first major step towards creating a current situation analysis of Coop by going to the store and conducting a Customer Safari, Customer Interviews and staff interviews. The insights of these methods were then used as the basis for the continued work of a workshop, brainstorming for solutions, a value map and service map that demonstrates the entire omni channel for Coop."
             secondText="The result of the work was a new experience trip for both customers and staff, where the digital service contributes to highlighting the value of an ecological and sustainable lifestyle."
           />
           <Card
-            pic="https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            pic="https://i0.wp.com/comintelli.com/wp-content/uploads/2015/07/Comintelli_Large-Box.png?fit=700%2C679&ssl=1"
             title="Internship - Comintelli AB "
             firstText="My internship at Comintelli AB consisted of a UX project where
           me and my colleague worked on trying to find a solution to
@@ -29,7 +29,7 @@ class Cards extends Component {
           result was a hi-fi prototype for the company."
           />
           <Card
-            pic="https://images.pexels.com/photos/796658/pexels-photo-796658.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            pic="https://yt3.ggpht.com/a-/AAuE7mA7nIjc5Y1hBa81uHc_wNi7foC5M6QpxzTeXw=s900-mo-c-c0xffffffff-rj-k-no"
             title="Sharp Design Project - Fondberg & Wincent"
             firstText="The project was about finding a basis for and creating a
           prototype application for questionnaire surveys on digital
@@ -42,7 +42,7 @@ class Cards extends Component {
           users to answer the questionnaire questions."
           />
           <Card
-            pic="https://images.pexels.com/photos/796658/pexels-photo-796658.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            pic="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Adobe_Illustrator_CC_icon.svg/1200px-Adobe_Illustrator_CC_icon.svg.png"
             title="Graphical Productions"
             firstText=" Here are some graphic productions such as logotypes,
              illustrations, sketches, prototypes, style documents and ideas."
