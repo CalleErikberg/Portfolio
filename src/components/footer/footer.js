7845af20f071ed936e696ca8305c9d992698181a
Erik Calleberg <erik.calleberg@gmail.com>
2020-03-03 14:57:28 +0100

    Revert "patch7"
    This reverts commit 035319c6a69f4bae30cd4dff72a4a1553ae5ef5c.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
       <div className="footer">
         <p>
           This site was made by Erik Calleberg using Gatsby.js Last updated:
-          03-03-2020 1:33 PM
+          15/07-2019 1:33 PM
         </p>
       </div>
     )
