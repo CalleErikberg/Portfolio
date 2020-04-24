aadf2548a95444481f0f8b5b7661c3421fc08d7e
Erik Calleberg <erik.calleberg@gmail.com>
2020-04-24 17:19:24 +0200

    mroe projects

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -8,7 +8,7 @@ class Cards extends Component {
       <div className="content" id="portfolio">
         <h1>Portfolio</h1>
         <p className="intro-portfolio">
-          Below are some my design projects I've contributed to, sorted into thumbnails. Click on them for more information details.
+          Click on one of the thumbnails to go the portfolio page for informations about the projects!
         </p>
         <div id="content">
         <Link to="/projects/">
@@ -58,6 +58,43 @@ class Cards extends Component {
             firstText=" Here are some graphic productions such as logotypes,
              illustrations, sketches, prototypes, style documents and ideas."
             secondText=""
+          />
+          </Link>
+          <Link to="/projects/">
+          <Card
+            index={4}
+            pic="https://imgur.com/1R12izQ"
+            title="GG.BUDDY"
+            firstText="This was a project requested by a friend to improve the layout of their idea to match people with random players on with each other for fast connections and a local voice chat." 
+            secondText="The project itself was cancelled but I got to work on web layouts which I think is really fun."
+          />
+          </Link>
+          <Link to="/projects/">
+          <Card
+            index={5}
+            pic="https://imgur.com/kcO4Gem"
+            title="Interactive Dance Floor - Rave Cave"
+            firstText="This was a project in my Master's programme in which the project group had as a task to create a interactive exhibition on the theme green enviroment and citizens. 
+            It ended up in as an interactive dance floor, and using Wizard of Oz (illusions) we tricked the user into thinking that they where actually generating power by dancing "
+            secondText="The result of the work was a new experience trip for the users and for us students to experiment with illusion techniques and Micro:bit+light programming."
+          />
+          </Link>
+          <Link to="/projects/">
+          <Card
+            index={6}
+            pic="https://media-eu.jobylon.com/CACHE/companies/company-logo/demo-sverige/coop_logo_sweden_green.9d2a1c4e/1849c21895bf81009bb55e97d2bfb751.jpg"
+            title="COOP - Service Design"
+            firstText="The work has been carried out for the food company Coop Sverige AB. The group started the project by participating in two teacher-led workshops to prepare the group members for the real work. The first major step towards creating a current situation analysis of Coop by going to the store and conducting a Customer Safari, Customer Interviews and staff interviews. The insights of these methods were then used as the basis for the continued work of a workshop, brainstorming for solutions, a value map and service map that demonstrates the entire omni channel for Coop."
+            secondText="The result of the work was a new experience trip for both customers and staff, where the digital service contributes to highlighting the value of an ecological and sustainable lifestyle."
+          />
+          </Link>
+          <Link to="/projects/">
+          <Card
+            index={7}
+            pic="https://media-eu.jobylon.com/CACHE/companies/company-logo/demo-sverige/coop_logo_sweden_green.9d2a1c4e/1849c21895bf81009bb55e97d2bfb751.jpg"
+            title="COOP - Service Design"
+            firstText="The work has been carried out for the food company Coop Sverige AB. The group started the project by participating in two teacher-led workshops to prepare the group members for the real work. The first major step towards creating a current situation analysis of Coop by going to the store and conducting a Customer Safari, Customer Interviews and staff interviews. The insights of these methods were then used as the basis for the continued work of a workshop, brainstorming for solutions, a value map and service map that demonstrates the entire omni channel for Coop."
+            secondText="The result of the work was a new experience trip for both customers and staff, where the digital service contributes to highlighting the value of an ecological and sustainable lifestyle."
           />
           </Link>
         </div>
