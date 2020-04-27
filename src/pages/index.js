2a8cd925544e819cc381aabed579bfde4ce99db8
Erik Calleberg <erik.calleberg@gmail.com>
2020-04-27 16:30:13 +0200

    remove

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "react-scroll"
 import Nav from "../components/nav/nav"
 import Intro from "../components/intro/intro"
-
 import AboutMe from "../components/aboutme/aboutme"
 import Contact from "../components/contact/contact"
 import Footer from "../components/footer/footer"
@@ -17,13 +16,6 @@ class IndexPage extends React.Component {
       <div>
         <title>Erik Calleberg - Portfolio</title>
         <head><link rel="shortcut icon" type="image/png" href={favicon}/>
-        <meta property="og:title" content="Erik Calleberg" />
-        <meta property="og:type" content="website" />
-<meta property="og:url" content="https://www.erikcalleberg.com" />
-<meta property="og:image" content="https://www.erikcalleberg.com/static/ec-7422492e904cee50cb0a8c4fd47fa7bd.png" />
-<meta HTTP-EQUIV="Pragma" CONTENT="no-cache"/>
-<meta HTTP-EQUIV="Expires" CONTENT="-10"/>
-
         </head>
       <BackToTop />
         <Nav />
