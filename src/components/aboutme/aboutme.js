6c66c665fd4502928ef9e753e3761df946ab5344
Erik Calleberg <erik.calleberg@gmail.com>
2020-05-26 19:19:21 +0200

    overflow y

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -42,19 +42,10 @@ class AboutMe extends Component {
         <div className="columnA3">
           <h2>An UX/UI Designer!</h2>
           <p>
-            I am 22 years old, raised in Stockholm where I currently live. My
+            I am 23 years old, raised in Stockholm where I currently live. My
             interests are computer games, orienteering, memes and an active
             outdoor lifestyle. I am currently a student at Södertörns Högskola studying 
             their Master's programme "User Experience and  Interactive Media Design"!
-            <br />
-            <br />
-            As for my experience with Design, the best part of design for me is
-            the journey from just an idea to a fully-fledged prototype to
-            deliver. Especially interesting are the UX/UI aspects of a project,
-            seeing the small details of e.g where a “Accept”-button goes and
-            why. Although my primary experience is Design I tend to return to
-            coding projects such as this website I made from scratch with Gatsby.js
-            and React.js.
           </p>
         </div>
       </div>
